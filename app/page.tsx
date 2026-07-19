98c5aa32f4b3bb484cbb58a845efea7df3f0f215
이민우 <alsdndjdrktl@gmail.com>
2026-07-19 14:37:31 +0900

    HeroUI 확인 버튼 제거와 진행 애니메이션 복원

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { Button } from "@heroui/react";
 import { Stack } from "../components/stackflow";
 
 export default function Home() {
   return (
     <main className="w-full h-[100dvh] overflow-hidden bg-white dark:bg-black">
       <Stack />
-      <Button className="fixed right-4 top-4 z-50">HeroUI ready</Button>
     </main>
   );
 }
